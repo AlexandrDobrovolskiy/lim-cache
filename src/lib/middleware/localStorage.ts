3b68b8d9eb87e748f5f8d9a1361154550c3b0b8d
Alexandr <sasha732sasha000@gmail.com>
2018-11-13 19:36:00 +0200

    fixed local storage data lose

--- src/lib/middleware/localStorage.ts
+++ src/lib/middleware/localStorage.ts
@@ -1,27 +1,32 @@
-import { IStorageMiddleware, LocalStorageMiddlewareConfig, IStorage } from "store.js";
-import _ from "lodash";
+import {
+  IStorageMiddleware,
+  LocalStorageMiddlewareConfig,
+  IStorage
+} from 'store.js';
+import _ from 'lodash';
 
 export class LocalStorageMiddleware implements IStorageMiddleware {
-  private name: string = "storage";
+  private name: string = 'storage';
   private blacklist: Array<string>;
 
-  constructor(config: LocalStorageMiddlewareConfig){
+  constructor(config: LocalStorageMiddlewareConfig) {
     this.name = config.name;
     this.blacklist = config.blacklist;
   }
 
   onSave(storage: IStorage): void {
-    localStorage.setItem(this.name, JSON.stringify(storage.getAll()));
-  }  
+    let data = JSON.stringify(storage.getAll());
+    localStorage.setItem(this.name, data);
+  }
 
   onLoad(storage: IStorage): void {
     const records = JSON.parse(localStorage.getItem(this.name));
-
-    _.forOwn(records, ({key}, val) => {
-      if(!this.blacklist.includes(key)){
-        storage.set(key, val);
+    _.forOwn(records, record => {
+      if (this.blacklist.includes(record.key)) {
+        return;
       }
-    })
+      storage.set(record.key, record.value);
+    });
   }
 
   onRemove(storage: IStorage): void {
